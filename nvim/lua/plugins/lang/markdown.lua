4e760323e8a0d2a71d3fbc753e072aeb6bec8f49
ABDO <contact.achak@gmail.com>
2025-05-05 10:42:03 +0100

    feat(nvim): markdown-preview

--- nvim/lua/plugins/lang/markdown.lua
+++ nvim/lua/plugins/lang/markdown.lua
@@ -229,4 +229,23 @@ return {
       }):map("<leader>um")
     end,
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+      vim.fn["mkdp#util#install"]()
+    end,
+    keys = {
+      {
+        "<leader>cp",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
+      },
+    },
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
+  },
 }
