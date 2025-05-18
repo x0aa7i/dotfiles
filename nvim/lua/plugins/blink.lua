657a90154cf54260b25c8527b3cbea6bb018c5f1
ABDO <contact.achak@gmail.com>
2025-05-18 23:25:04 +0100

    fix(nvim): remove problematic colorful-menu dependency

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,8 +1,5 @@
 return {
   "saghen/blink.cmp",
-  dependencies = {
-    "xzbdmw/colorful-menu.nvim",
-  },
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
@@ -39,16 +36,6 @@ return {
           align_to = "label",
           treesitter = { "lsp" },
           columns = { { "kind_icon" }, { "label", gap = 1 } },
-          components = {
-            label = {
-              text = function(ctx)
-                return require("colorful-menu").blink_components_text(ctx)
-              end,
-              highlight = function(ctx)
-                return require("colorful-menu").blink_components_highlight(ctx)
-              end,
-            },
-          },
         },
       },
       ghost_text = { enabled = false },
