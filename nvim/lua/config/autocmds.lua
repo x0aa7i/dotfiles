fd08fe1fdd5b1947a3f435d2238b7111795417ab
ABDO <contact.achak@gmail.com>
2025-07-05 18:23:13 +0100

    feat(nvim): add buffer cleanup and refactor code

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,43 +2,53 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Disable diagnostic for .env files
-vim.api.nvim_create_autocmd("BufReadPost", {
-  pattern = { "*.env", ".env.*" },
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+autocmd("BufReadPost", {
   desc = "Disable diagnostic for .env files",
+  pattern = { "*.env", ".env.*" },
   callback = function(ev)
     vim.diagnostic.enable(false, { bufnr = ev.buf })
   end,
 })
 
--- Remove '-' from iskeyword
-vim.api.nvim_create_autocmd({ "BufEnter", "BufReadPost" }, {
+autocmd({ "BufEnter", "BufReadPost" }, {
+  desc = "Remove '-' from iskeyword",
   pattern = "*",
   callback = function()
     vim.opt_local.iskeyword:remove("-")
   end,
 })
 
--- Trim whitespaces
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  group = vim.api.nvim_create_augroup("trim_whitespaces", { clear = true }),
-  pattern = { "*" },
-  desc = "Trim trailing whitespaces",
+autocmd("BufWritePre", {
+  desc = "Remove all trailing whitespace on save",
   callback = function()
     local save_cursor = vim.fn.getpos(".")
     vim.cmd([[%s/\s\+$//e]])
     vim.fn.setpos(".", save_cursor)
   end,
+  group = augroup("TrimWhiteSpaceGrp", { clear = true }),
 })
 
--- Replace curly single quotes with straight single quotes in Markdown files
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  group = vim.api.nvim_create_augroup("replace_curly_single_quotes", { clear = true }),
-  pattern = { "*.md", "*.markdown" },
-  desc = "Replace curly single quotes in markdown files",
+autocmd("BufWritePre", {
+  desc = "Replace smart quotes and apostrophes in Markdown files",
+  pattern = { "*.md" },
   callback = function()
     local save_cursor = vim.fn.getpos(".")
-    vim.cmd([[silent! %s/’/'/ge]])
+    vim.cmd([[%s/[“”]/"/ge | %s/’/'/ge]])
     vim.fn.setpos(".", save_cursor)
+  end,
+  group = augroup("MarkdownCleanupGrp", { clear = true }),
+})
+
+-- Delete [No Name] buffers
+autocmd("BufHidden", {
+  callback = function(event)
+    if event.file == "" and vim.bo[event.buf].buftype == "" and not vim.bo[event.buf].modified then
+      vim.schedule(function()
+        pcall(vim.api.nvim_buf_delete, event.buf, {})
+      end)
+    end
   end,
 })
