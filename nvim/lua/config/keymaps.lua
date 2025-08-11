0a5abf9d1ca4b83e350f0229eaf7787179fbf70b
ABDO <contact.achak@gmail.com>
2025-08-12 00:05:29 +0100

    feat(nvim): Rework keymap utility and add new mappings

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,78 +7,98 @@ if vim.g.vscode then -- use vscode keymaps instead
 end
 
 -- Quit
-K.map("<C-q>", "<cmd>qa<cr>", { desc = "Quit all" })
+K.map("n", "<C-q>", "<cmd>qa<cr>", { desc = "Quit all" })
 
 -- Better Search navigation
-K.map("n", "nzz")
-K.map("N", "Nzz")
-K.map("*", "*zz")
-K.map("#", "#zz")
-K.map("g*", "g*zz")
-K.map("g#", "g#zz")
+K.map("n", "n", "nzz")
+K.map("n", "N", "Nzz")
+K.map("n", "*", "*zz")
+K.map("n", "#", "#zz")
+K.map("n", "g*", "g*zz")
+K.map("n", "g#", "g#zz")
 
 -- Press jk fast to enter normal mode
-K.map("jk", "<ESC>", { mode = "i", desc = "Exit insert mode" })
+K.map("i", "jk", "<ESC>", { desc = "Exit insert mode" })
 
-K.map("<A-d>", [[m`"yyy"yp``j]], { mode = "n", desc = "Duplicate line" })
-K.map("<A-d>", [[<Esc>"yyy"ypgi]], { mode = "i", desc = "Duplicate line" })
-K.map("<A-d>", [["yy'>"ypgv]], { mode = "v", desc = "Duplicate selection" })
+K.map("n", "<A-d>", [[m`"yyy"yp``j]], { desc = "Duplicate line" })
+K.map("i", "<A-d>", [[<Esc>"yyy"ypgi]], { desc = "Duplicate line" })
+K.map("v", "<A-d>", [["yy'>"ypgv]], { desc = "Duplicate selection" })
 
 -- select all
-K.map("<C-S-a>", "<esc>ggVG", { mode = { "i", "n", "v" } })
+K.map({ "i", "n", "v" }, "<C-S-a>", "<esc>ggVG")
+
+-- paste system clipboard
+K.map("i", "<C-v>", "<C-r>+", { desc = "Paste" })
 
 -- Move to start/end of line
-K.map("H", "^", { mode = { "n", "x" } })
-K.map("L", "$", { mode = { "n", "x" } })
+K.map({ "n", "x" }, "H", "^")
+K.map({ "n", "x" }, "L", "$")
 
 -- Delete word with backspace
-K.map("<C-BS>", "<C-w>", { mode = "i" })
-K.map("<C-Del>", [[<esc>"xcw]], { mode = "i" })
+K.map("i", "<C-BS>", "<C-w>")
+K.map("i", "<C-Del>", [[<esc>"xcw]])
 
-K.map("<CR>", [["xciw]], { mode = "n", desc = "Change inner word" })
-K.map("<CR>", [["xc]], { mode = "v", desc = "Change selection" })
+K.map("n", "<CR>", [["xciw]], { desc = "Change inner word" })
+K.map("v", "<CR>", [["xc]], { desc = "Change selection" })
 
-K.map("gl", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+-- Diagnostics
+K.map("n", "gl", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+
+-- join lines and maintain cursor position
+local join_lines = function()
+  local pos = vim.api.nvim_win_get_cursor(0)
+  vim.cmd([[normal! J]])
+  vim.api.nvim_win_set_cursor(0, pos)
+end
+
+K.map("n", "J", join_lines, { desc = "Join lines" })
 
 -- Folds
-K.map("z1", "<cmd> setlocal foldlevel=0<cr>", { mode = "n", desc = "Fold Level 1" })
-K.map("z2", "<cmd> setlocal foldlevel=1<cr>", { mode = "n", desc = "Fold Level 2" })
-K.map("z3", "<cmd> setlocal foldlevel=2<cr>", { mode = "n", desc = "Fold Level 3" })
-K.map("z4", "<cmd> setlocal foldlevel=3<cr>", { mode = "n", desc = "Fold Level 4" })
-K.map("z5", "<cmd> setlocal foldlevel=4<cr>", { mode = "n", desc = "Fold Level 5" })
+K.map("n", "z1", "<cmd> setlocal foldlevel=0<cr>", { desc = "Fold Level 1" })
+K.map("n", "z2", "<cmd> setlocal foldlevel=1<cr>", { desc = "Fold Level 2" })
+K.map("n", "z3", "<cmd> setlocal foldlevel=2<cr>", { desc = "Fold Level 3" })
+K.map("n", "z4", "<cmd> setlocal foldlevel=3<cr>", { desc = "Fold Level 4" })
+K.map("n", "z5", "<cmd> setlocal foldlevel=4<cr>", { desc = "Fold Level 5" })
 
 -- windows
-K.map("<leader>wq", "<C-W>c", { desc = "Delete Window", remap = true })
-K.map("<leader>ws", "<C-W>s", { desc = "Split Window Below", remap = true })
-K.map("<leader>wv", "<C-W>v", { desc = "Split Window Right", remap = true })
+K.map("n", "<leader>wq", "<C-W>c", { desc = "Delete Window", remap = true })
+K.map("n", "<leader>ws", "<C-W>s", { desc = "Split Window Below", remap = true })
+K.map("n", "<leader>wv", "<C-W>v", { desc = "Split Window Right", remap = true })
 
 -- Buffers
-K.map("Q", function()
+K.map("n", "Q", function()
   Snacks.bufdelete()
 end, { desc = "Delete Buffer" })
 
 -- tabs
-K.map("<leader><tab>q", "<cmd>tabclose<cr>", { desc = "Close Tab" })
-K.map("]<tab>", "<cmd>tabnext<cr>", { desc = "Next Tab" })
-K.map("[<tab>", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+K.map("n", "<leader><tab>q", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+K.map("n", "]<tab>", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+K.map("n", "[<tab>", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
 
 -- Comment
-K.map("<c-/>", "<Nop>") -- Remove terminal keybind set by lazyvim
+K.map("n", "<c-/>", "<Nop>") -- Remove terminal keybind set by lazyvim
 
-K.map("<c-/>", "gcc", { mode = "n", desc = "comment toggle", remap = true })
-K.map("<c-/>", "gc", { mode = "v", desc = "comment toggle", remap = true })
+K.map("n", "<c-/>", "gcc", { desc = "comment toggle", remap = true })
+K.map("v", "<c-/>", "gc", { desc = "comment toggle", remap = true })
 
-K.map("<c-_>", "gcc", { mode = "n", desc = "comment toggle", remap = true })
-K.map("<c-_>", "gc", { mode = "v", desc = "comment toggle", remap = true })
+K.map("n", "<c-_>", "gcc", { desc = "comment toggle", remap = true })
+K.map("v", "<c-_>", "gc", { desc = "comment toggle", remap = true })
 
+-- Copy file path to clipboard
+local copy_file_path_to_clipboard = function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify("Copied " .. path .. " to clipboard")
+end
+K.map("n", "<leader>cp", copy_file_path_to_clipboard, { desc = "Copy file path to clipboard" })
 
 -- Terminal
 -- stylua: ignore
 local terminal = function() Snacks.terminal.toggle() end
-K.map("<c-\\>", terminal, { mode = { "n", "t" }, desc = "Terminal (Root Dir)" })
+K.map({ "n", "t" }, "<c-\\>", terminal, { desc = "Terminal (Root Dir)" })
 
 -- Google search
 local searching_google_in_visual =
   [[<ESC>gv"gy<ESC>:lua vim.fn.system({'xdg-open', 'https://google.com/search?q=' .. vim.fn.getreg('g')})<CR>]]
 
-K.map("gx", searching_google_in_visual, { mode = "v", silent = true, noremap = true })
+K.map("v", "gx", searching_google_in_visual, { silent = true, noremap = true })
--- nvim/lua/utils/init.lua
+++ nvim/lua/utils/init.lua
@@ -1,1 +1,29 @@
-require("utils.keymap")
+K = {}
+
+local default_map_opts = {
+  silent = true,
+  remap = false,
+}
+
+---@alias Mode "n"|"v"|"i"|"c"|"s"|"o"|"t"|"x"
+
+---@class Opts
+---@field desc string|nil Description for the keymap
+---@field silent boolean|nil Suppress command output
+---@field buffer integer|nil Buffer number (if buffer-local keymap)
+---@field remap boolean|nil Allow remapping
+---@field nowait boolean|nil Do not wait for a character in a mapping
+---@field expr boolean|nil The RHS is an expression
+---@field unique boolean|nil Throw an error if mapping already exists
+
+--- Map a key
+---@param mode Mode|Mode[] Keymap mode(s)
+---@param key string Key to be mapped
+---@param cmd string|function Action to be taken
+---@param options? Opts Options for the keymap
+---
+---@return nil
+function K.map(mode, key, cmd, options)
+  local opts = vim.tbl_extend("force", default_map_opts, options or {})
+  vim.keymap.set(mode, key, cmd, opts)
+end
